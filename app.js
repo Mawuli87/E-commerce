baa62b0e36142acb3349e3a4d4158f943ac51175
Mawuli-mcc <mawuli@mobilecontent.com.gh>
2023-12-01 18:43:05 +0000

    Emmerce app server code

---
+++ app.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { config } from "dotenv";
+import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
+config({
+    path: "./data/config.env",
+  });
+
+  export const app = express();
+  app.use(cookieParser());
+  app.use(
+    cors({
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      origin: [process.env.FRONTEND_URI_1, process.env.FRONTEND_URI_2],
+    })
+  );
+
+  // Using Middlewares
+app.use(express.json());
+
+  app.get("/",(req,res,next) => {
+    res.send("Working with Mawuli");
+  })
+
+  //importing routes
+  import user from "./routes/user.js";
+  import product from "./routes/product.js";
+  import order from "./routes/order.js";
+ 
+
+  app.use("/api/v1/user", user);
+  app.use("/api/v1/product", product);
+  app.use("/api/v1/order", order);
+
+
+
+  // Using Error Middleware
+app.use(errorMiddleware);
+
---
+++ routes/user.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { changePassword, forgetpassword, getMyProfile, logOut, login, resetpassword, signup, updatePic, updateProfile } from "../controllers/user.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+import { singleUpload } from "../middlewares/multer.js";
+
+const router = express.Router();
+
+router.post("/login",login);
+
+router.post("/register",singleUpload,signup);
+
+router.get("/me", isAuthenticated, getMyProfile);
+
+router.get("/logout", isAuthenticated, logOut);
+
+//updating routes
+router.put("/updateprofile", isAuthenticated, updateProfile);
+router.put("/changepassword", isAuthenticated, changePassword);
+router.put("/updatepic", isAuthenticated, singleUpload, updatePic);
+
+// Forget Password & Reset Password
+router.route("/forgetpassword").post(forgetpassword).put(resetpassword);
+
+
+export default router;
